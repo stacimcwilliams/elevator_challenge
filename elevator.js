8bbb7a1e129ff064ce02d7e0a7f3c63f736ba5c4
Staci McWilliams <stacinicole86@gmail.com>
2017-05-10 12:15:48 -0600

    Testing

--- elevator.js
+++ elevator.js
@@ -1,7 +1,91 @@
-export default class Elevator {
-  constructor() {
+class Elevator {
+  constructor(currentFloor) {
+    this.currentFloor = 0,
+    this.currentRiders = [],
+    this.motionStatus = 'idle' || 'moving' || 'broken',
+    this.requests = [],
+    this.totalStops = 0,
+    this.totalFloors = 0
   }
 
-  get currentFloor() {
+
+  currentFloor() {
+    this.currentFloor
   }
+
+  floorRequest(person) {
+    this.requests.push({
+      name: person.name,
+      currentFloor: person.currentFloor,
+      requestedFloor : person.requestedFloor
+    })
+    var floor = this.requests[0].requestedFloor
+
+    this.addFloorTotal(floor)
+    this.state = 'moving',
+    this.addStop()
+    this.currentFloor = floor
+  }
+
+  reset() {
+    this.currentFloor = 0
+    this.motionStatus = 'idle'
+    this.currentRiders = []
+  }
+
+  pickUpRider(person, requestedFloor) {
+    let floor = person.currentFloor
+    this.addStop()
+    this.status = 'moving'
+    this.currentFloor = person.currentFloor
+    this.currentRiders.push(person)
+    this.floorRequest(person, requestedFloor)
+    this.addFloorTotal(floor)
+  }
+
+  addStop() {
+    this.motionStatus = 'idle'
+    this.totalStops = this.totalStops +1
+  }
+
+  addFloorTotal(floor) {
+    this.totalFloors += Math.abs(this.currentFloor - floor)
+  }
+
+  getStops(currentFloor, requestedFloor) {
+
+  }
+
 }
+
+class Person {
+  constructor(name, currentFloor, requestedFloor) {
+    this.name = name,
+    currentFloor = currentFloor,
+    requestedFloor = requestedFloor
+  }
+
+}
+
+module.exports = {Elevator, Person};
+
+
+// Vending Machine Example
+//setSelection(keycode) {
+//selection = keycode
+//}
+
+//credits> inventory.keycode.cost
+
+//insertCredits(amt)
+//credits += amt
+
+//vend(keycode)
+//need a status method in the constructor
+//check credits and inventory if they both pass vend otherwise display status code (not enough credit or out of stock)
+
+//vend(keycode)
+//selection = null
+//inventory.keycode .shift()
+//credit -= inventory.keycode.constructor
+//credits > 0 return credits
--- tests/elevator_test.js
+++ tests/elevator_test.js
@@ -1,10 +1,13 @@
+const assert = require('chai').assert
+
 require('babel-core/register')({
   ignore: /node_modules\/(?!ProjectB)/
 });
 
-const Elevator = require('../elevator').default;
+const { Elevator, Person } = require('../elevator');
 
 describe('Elevator', function() {
+
   let elevator = new Elevator();
 
   afterEach(function() {
@@ -12,20 +15,51 @@ describe('Elevator', function() {
   });
 
   it('should bring a rider to a floor above their current floor', () => {
-    let mockUser = { name: "Brittany", currentFloor: 2, dropOffFloor: 5 };
-    elevator.goToFloor(mockUser);
 
-    assert(elevator.currentFloor).equals(5);
-    assert(elevator.motionStatus).equals('idle');
-    assert(elevator.getStops()).equals([2, 5]);
-  });
+    let person = new Person("staci", 5)
+
+    assert.equal(elevator.currentFloor, 0)
+    elevator.pickUpRider(person, 5);
+
+    assert.deepEqual(elevator.currentRiders, [person])
+    assert.equal(elevator.motionStatus, 'idle');
+    assert.equal(elevator.currentFloor, 5)
+    assert.equal(elevator.totalStops, 2)
+    console.log(elevator.getStops());
+    // assert(elevator.getStops()).equals([2, 5]);
+    // assert.equal(elevator.addFloorTotal(5), 5)
+  })
 
   it('should bring a rider to a floor below their current floor', () => {
-    let mockUser = { name: "Brittany", currentFloor: 8, dropOffFloor: 3 };
-    elevator.goToFloor(mockUser);
+    let person = new Person("Brittany", 5, 6)
+    assert.equal(elevator.currentFloor, 0)
 
-    assert(elevator.currentFloor).equals(3);
-    assert(elevator.motionStatus).equals('idle');
-    assert(elevator.getStops()).equals([8, 3]);
+    elevator.floorRequest(person);
+    elevator.pickUpRider(person)
+    assert.equal(elevator.currentRiders[0].name, "Brittany")
+    // assert.equal(elevator.currentFloor, 5)
+    // assert(elevator.motionStatus).equals('idle');
+    // assert(elevator.getStops()).equals([8, 3]);
   });
+
+it('it should have a reset method', () => {
+  let person = new Person("Brittany", 5)
+  elevator.floorRequest(person,3)
+  assert.equal(elevator.currentFloor, 0)
+})
+
+it('should have a currentFloor method', () => {
+  let person = new Person("Brittany", 5)
+  assert.equal(elevator.currentFloor, 0)
+  elevator.floorRequest(person, 4)
+})
 });
+
+describe('Person', function() {
+    let person = new Person("Louisa", 7, 9)
+
+    it('should have a person with a name and current floor', () => {
+      assert.deepEqual(person.name, "Louisa")
+      assert.equal(person.currentFloor, 7)
+    })
+})
