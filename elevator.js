2a936263b12cd8c717a93fe23b9a5b13ea3cc5bd
Staci McWilliams <stacinicole86@gmail.com>
2017-05-12 10:42:50 -0600

    Add elevator tests, all tests passing, refactor code for elevator.js file

--- elevator.js
+++ elevator.js
@@ -1,91 +1,98 @@
 class Elevator {
-  constructor(currentFloor) {
+  constructor() {
     this.currentFloor = 0,
     this.currentRiders = [],
     this.motionStatus = 'idle' || 'moving' || 'broken',
-    this.requests = [],
     this.totalStops = 0,
-    this.totalFloors = 0
+    this.totalFloors = 0,
+    this.goingUp = true,
+    this.requests = []
   }
 
+  pickUpPerson(person) {
+    if(person.requestedFloor != this.currentFloor) {
+      this.currentRiders.push(person)
+      this.motionStatus = "moving"
+    }
+
+    if(this.currentRiders.length == 1) {
+      if(person.currentFloor > this.currentFloor) {
+        this.goingUp = false
+      } else if (person.currentFloor > this.currentFloor) {
+        this.goingUp = true
+      }
+    }
+  }
+
+  dropOffPerson(person) {
+    var riderIndex = this.currentRiders.indexOf(person);
+    this.currentRiders.splice(riderIndex, 1);
+  }
+
+  goToFloor() {
+    var self = this
+
+    while (this.currentRiders.length) {
+      let stop = false;
+
+      if (this.goingUp) {
+        this.currentFloor++;
+      } else {
+        this.currentFloor--;
+      }
+
+      this.currentRiders.forEach(function(myRider, index, myArray) {
+        myArray[index].currentFloor = self.currentFloor;
+        if (myRider.requestedFloor == self.currentFloor) {
+          self.dropOffPerson(myRider)
+          stop = true;
+        }
+      })
+
+      if (stop) {
+        this.totalStops++;
+      }
+
+      this.totalFloors++
+
+      if (this.goingUp) {
+        if (!this.currentRiders.find(function(rider, index){
+          return rider.requestedFloor > self.currentFloor
+        })) {
+          this.goingUp = false;
+        }
+      } else {
+        if (!(this.currentRiders.find(function(rider, index){
+          return (rider.requestedFloor < self.currentFloor)
+        }))) {
+          this.goingUp = true;
+        }
+      }
+    }
+    this.motionStatus = 'idle'
+  }
+
+  reset() {
+    this.currentFloor = 0,
+    this.motionStatus = 'idle'
+    this.currentRiders = [],
+    this.totalStops = [],
+    this.totalFloors = 0,
+    this.requests = []
+  }
 
   currentFloor() {
     this.currentFloor
   }
 
-  floorRequest(person) {
-    this.requests.push({
-      name: person.name,
-      currentFloor: person.currentFloor,
-      requestedFloor : person.requestedFloor
-    })
-    var floor = this.requests[0].requestedFloor
-
-    this.addFloorTotal(floor)
-    this.state = 'moving',
-    this.addStop()
-    this.currentFloor = floor
-  }
-
-  reset() {
-    this.currentFloor = 0
-    this.motionStatus = 'idle'
-    this.currentRiders = []
-  }
-
-  pickUpRider(person, requestedFloor) {
-    let floor = person.currentFloor
-    this.addStop()
-    this.status = 'moving'
-    this.currentFloor = person.currentFloor
-    this.currentRiders.push(person)
-    this.floorRequest(person, requestedFloor)
-    this.addFloorTotal(floor)
-  }
-
-  addStop() {
-    this.motionStatus = 'idle'
-    this.totalStops = this.totalStops +1
-  }
-
-  addFloorTotal(floor) {
-    this.totalFloors += Math.abs(this.currentFloor - floor)
-  }
-
-  getStops(currentFloor, requestedFloor) {
-
-  }
-
 }
 
 class Person {
-  constructor(name, currentFloor, requestedFloor) {
+  constructor({name, currentFloor, requestedFloor}) {
     this.name = name,
-    currentFloor = currentFloor,
-    requestedFloor = requestedFloor
+    this.currentFloor = currentFloor,
+    this.requestedFloor = requestedFloor
   }
-
 }
 
 module.exports = {Elevator, Person};
-
-
-// Vending Machine Example
-//setSelection(keycode) {
-//selection = keycode
-//}
-
-//credits> inventory.keycode.cost
-
-//insertCredits(amt)
-//credits += amt
-
-//vend(keycode)
-//need a status method in the constructor
-//check credits and inventory if they both pass vend otherwise display status code (not enough credit or out of stock)
-
-//vend(keycode)
-//selection = null
-//inventory.keycode .shift()
-//credit -= inventory.keycode.constructor
-//credits > 0 return credits
--- tests/elevator_test.js
+++ tests/elevator_test.js
@@ -14,52 +14,67 @@ describe('Elevator', function() {
     elevator.reset();
   });
 
-  it('should bring a rider to a floor above their current floor', () => {
+  it('should bring a rider to a floor above their current floor', (done) => {
+    let person = new Person({ name: "Hilary", currentFloor: 4, requestedFloor: 5 })
+    elevator.pickUpPerson(person);
+    elevator.goToFloor();
 
-    let person = new Person("staci", 5)
-
-    assert.equal(elevator.currentFloor, 0)
-    elevator.pickUpRider(person, 5);
-
-    assert.deepEqual(elevator.currentRiders, [person])
-    assert.equal(elevator.motionStatus, 'idle');
-    assert.equal(elevator.currentFloor, 5)
-    assert.equal(elevator.totalStops, 2)
-    console.log(elevator.getStops());
+    assert.equal(person.currentFloor, 5)
+    done()
+    // elevator.pickUpRider(person, 5);
+    //
+    // assert.deepEqual(elevator.currentRiders, [person])
+    // assert.equal(elevator.motionStatus, 'idle');
+    // assert.equal(elevator.currentFloor, 5)
+    // assert.equal(elevator.totalStops, 2)
+    // console.log(elevator.getStops());
     // assert(elevator.getStops()).equals([2, 5]);
     // assert.equal(elevator.addFloorTotal(5), 5)
   })
 
-  it('should bring a rider to a floor below their current floor', () => {
-    let person = new Person("Brittany", 5, 6)
-    assert.equal(elevator.currentFloor, 0)
-
-    elevator.floorRequest(person);
-    elevator.pickUpRider(person)
-    assert.equal(elevator.currentRiders[0].name, "Brittany")
-    // assert.equal(elevator.currentFloor, 5)
-    // assert(elevator.motionStatus).equals('idle');
-    // assert(elevator.getStops()).equals([8, 3]);
+  it('should bring a rider to a floor below their current floor', (done) => {
+    let person = new Person({ name: "Hilary", currentFloor: 4, requestedFloor: 1 })
+    elevator.pickUpPerson(person);
+    elevator.goToFloor()
+    assert.equal(elevator.currentFloor, 1)
+    done()
   });
 
 it('it should have a reset method', () => {
-  let person = new Person("Brittany", 5)
-  elevator.floorRequest(person,3)
+  elevator.motionStatus = "moving"
+  elevator.currentFloor = 20
+  elevator.reset()
   assert.equal(elevator.currentFloor, 0)
 })
 
 it('should have a currentFloor method', () => {
   let person = new Person("Brittany", 5)
   assert.equal(elevator.currentFloor, 0)
-  elevator.floorRequest(person, 4)
 })
+
+it('should have a method called pickUpPerson that adds a rider to the array', () => {
+  let person = new Person({ name: "Hilary", currentFloor: 4, requestedFloor: 1 })
+  assert.deepEqual(elevator.currentRiders, [])
+  elevator.pickUpPerson(person)
+  assert.deepEqual(elevator.currentRiders[{ name: "Hilary", currentFloor: 4, requestedFloor: 1 }])
 });
 
-describe('Person', function() {
-    let person = new Person("Louisa", 7, 9)
-
-    it('should have a person with a name and current floor', () => {
-      assert.deepEqual(person.name, "Louisa")
-      assert.equal(person.currentFloor, 7)
-    })
+it('should have a method called dropOffPerson that removes a rider from the array', () => {
+  let person = new Person({ name: "Hilary", currentFloor: 4, requestedFloor: 1 })
+  elevator.pickUpPerson(person)
+  elevator.dropOffPerson()
+  assert.deepEqual(elevator.currentRiders, [])
+  // assert.deepEqual(elevator.currentRiders, [{ name: "Hilary", currentFloor: 4, requestedFloor: 1 }])
 })
+
+it('should have an attribute called totalStops that returns the total number of stops', () => {
+  let chelsea = new Person({ name: "Chelsea", currentFloor: 2, requestedFloor: 5 })
+  let hillary = new Person({ name: "Hillary", currentFloor: 4, requestedFloor: 1 })
+  let bill = new Person({ name: "Bill", currentFloor: 3, requestedFloor: 1 })
+  elevator.pickUpPerson(chelsea);
+  elevator.pickUpPerson(hillary);
+  elevator.pickUpPerson(bill);
+  elevator.goToFloor()
+  assert.equal(elevator.totalStops, 3)
+})
+});
